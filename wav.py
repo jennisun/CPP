b6934b1dadac6261a54e4383861b3c1a70340a7b
jennisun <jsun20@stuy.edu>
2023-02-22 15:04:31 -0500

    plot function

--- wav.py
+++ wav.py
@@ -2,8 +2,9 @@ import soundfile
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-file_path = "sound_files/car.wav"
+file_path = "sound_files/chord.wav"
 
 # Read and rewrite the file with soundfile
 data, samplerate = soundfile.read(file_path)
@@ -19,20 +20,27 @@ def spectrum(data):
     return f
 
 def cepstrum(data):
+    # convert to spectral domain
     s = spectrum(data)
+    # take the log
+    for index, d in enumerate(s):
+        s[index] = math.log(d)
+    # inverse fourier transform into cepstral domain
+    c = abs(np.fft.ifft(s))
+    return c
 
-def plot(data):
+def plot(data, func):
     data = convert(data)
 
-    N = len(data)
-    T = samplerate / len(data)
+    N = len(data) # size of data
+    T = samplerate # sampling rate
     x = np.linspace(0.0, N*T, N)
     y = data
-    yf = spectrum(y)
+    yf = func(y)
     xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
 
     fig, ax = plt.subplots()
     ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
     plt.show()
 
-plot(data)
+plot(data, cepstrum)
