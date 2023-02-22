4ad002c58715af66a6809b0ab47e95ae0a50521e
jennisun <jsun20@stuy.edu>
2023-02-22 14:45:26 -0500

    started spectrum, cepstrum

--- wav.py
+++ wav.py
@@ -1,19 +1,38 @@
 import soundfile
 import wave
 import numpy as np
+import matplotlib.pyplot as plt
 
 file_path = "sound_files/car.wav"
 
 # Read and rewrite the file with soundfile
 data, samplerate = soundfile.read(file_path)
 
-sound = []
-for i in data:
-    sound.append(i[1])
+def convert(data):
+    d = []
+    for i in data:
+        d.append(i[0])
+    return d
 
-def spectrum(sound):
-    f = math.abs(np.fft.fft(sound))
+def spectrum(data):
+    f = abs(np.fft.fft(data))
     return f
 
-def cepstrum(sound):
-    s = spectrum(sound)
+def cepstrum(data):
+    s = spectrum(data)
+
+def plot(data):
+    data = convert(data)
+
+    N = len(data)
+    T = samplerate / len(data)
+    x = np.linspace(0.0, N*T, N)
+    y = data
+    yf = spectrum(y)
+    xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+
+    fig, ax = plt.subplots()
+    ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
+    plt.show()
+
+plot(data)
